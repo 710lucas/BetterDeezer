eb7be7cd2777d095708e92326f2a0a1fba38d5ec
LucasPB710 <lucasperonicobarbotin@gmail.com>
2021-01-23 12:13:38 -0300

    primeiro commit, ainda esta WIP

---
+++ content.js
@@ -0,0 +1,25 @@
+
+
+function teste(){
+
+	var id = window.location.href.replace('https://www.deezer.com/br/album/','');
+	var dir = window.document.getElementsByClassName("_2kEwD ellipsis");
+
+	var url = 'https://raw.githubusercontent.com/LucasPB710/GeradorDeLingua/master/teste.txt';
+	var text = "a";
+
+	fetch(url).then(function(response){
+		response.text().then(function(_text){
+			text = _text;
+			dir[0].insertAdjacentHTML('afterend',"<p>"+text+"</p>");
+		});
+	});
+
+}
+window.onload = function(){
+//alert("DEEZER");
+//alert(url);
+
+setTimeout(teste,2000);
+
+}
