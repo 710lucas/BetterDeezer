f1700abf30e834288da5e653114ec1827c99af32
LucasPB710 <lucasperonicobarbotin@gmail.com>
2021-02-03 22:09:03 -0300

    Organizando estrutura dos albuns para o projeto.
    Muitos albuns futuramente iriam ser muitas pastas
    dentro da pasta raiz, por isso foi criado a pasta
    albuns, todos os albuns ficarão lá, pois descrição
    sobre artistas e musicas podem surgir futuramente
    tudo depende de, ou minha preguiça, ou a vontade de
    alguem de contribuir com essa extensão.

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ function teste(){
 	var id = window.location.href.replace('https://www.deezer.com/br/album/','');
 	var dir = window.document.getElementsByClassName("_2kEwD ellipsis");
 
-	var url = 'https://raw.githubusercontent.com/LucasPB710/BetterDeezer/master/'+id+'/desc.txt';
+	var url = 'https://raw.githubusercontent.com/LucasPB710/BetterDeezer/master/albuns/'+id+'/desc.txt';
 	var text = "a";
 
 	fetch(url).then(function(response){
