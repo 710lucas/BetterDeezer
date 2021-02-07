ac491f77caed7bb66cb8395652b0adea9a452a61
LucasPB710 <lucasperonicobarbotin@gmail.com>
2021-02-07 16:56:16 -0300

    adicionando habilidade de adicionar descrição para artistas

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
-//Função que executa a parte mais importante do codigo
-function BetterDeezer(){
+//Função que executa a parte dos albuns do codigo
+function Album(){
 
   //usado para pegar apenas o id do album
 	var id = window.location.href.replace('https://www.deezer.com/br/album/','');
@@ -21,9 +21,37 @@ function BetterDeezer(){
 
 }
 
+function Artist(){
+
+  //usado para pegar apenas o id d artist
+	var id = window.location.href.replace('https://www.deezer.com/br/artist/','');
+
+	//pega a classe que está abaixo da quantidade de seguidores
+	var dir = window.document.getElementsByClassName("_2kEwD ellipsis");
+
+  //Procura no repositorio do github um desc.txt na pasta com o id d artist
+	var url = 'https://raw.githubusercontent.com/LucasPB710/BetterDeezer/master/artistas/'+id+'/desc.txt';
+	var text = "a";
+
+  //Coloca o texto no site
+	fetch(url).then(function(response){
+		response.text().then(function(_text){
+			text = _text;
+			dir[0].insertAdjacentHTML('afterend',"<p>"+text+"</p>");
+		});
+	});
+
+}
+
 //Quando a pagina tiver carregado, inicia as funções
 window.onload = function(){
-  setTimeout(BetterDeezer,2000);
+  if(window.location.href.includes("album"))
+    setTimeout(Album,2000);
+  else if(window.location.href.includes("artist"))
+    setTimeout(Artist,2000);
+  else {
+    setTimeout(alert(1),2000);
+  }
   setTimeout(declarar,200);
 }
 
